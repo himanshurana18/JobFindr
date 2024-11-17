cce817bba9b06ddc939a8ae641a96e08ba8134e6
maclinz <tafadzwamucheke@gmail.com>
2024-11-17 21:45:48 +0000

    Bookmark

--- client/Components/JobItem/MyJob.tsx
+++ client/Components/JobItem/MyJob.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { Job } from "@/types/types";
 import { useJobsContext } from "@/context/jobsContext";
 import Image from "next/image";
@@ -10,36 +10,64 @@ import { Button } from "../ui/button";
 import { Pencil, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useGlobalContext } from "@/context/globalContext";
+import { bookmark, bookmarkEmpty } from "@/utils/Icons";
 
 interface JobProps {
   job: Job;
 }
 
 function MyJob({ job }: JobProps) {
-  const { deleteJob } = useJobsContext();
-  const { userProfile } = useGlobalContext();
+  const { deleteJob, likeJob } = useJobsContext();
+  const { userProfile, isAuthenticated } = useGlobalContext();
+  const [isLiked, setIsLiked] = React.useState(false);
 
   const router = useRouter();
+
+  const handleLike = (id: string) => {
+    setIsLiked((prev) => !prev);
+    likeJob(id);
+  };
+
+  useEffect(() => {
+    setIsLiked(job.likes.includes(userProfile._id));
+  }, [job.likes, userProfile._id]);
+
   return (
     <div className="p-8 bg-white rounded-xl flex flex-col gap-5">
-      <div
-        className="flex items-center space-x-4 mb-2 cursor-pointer"
-        onClick={() => router.push(`/job/${job._id}`)}
-      >
-        <Image
-          alt={`logo`}
-          src={job.createdBy.profilePicture || "/user.png"}
-          width={48}
-          height={48}
-          className="rounded-full shadow-sm"
-        />
+      <div className="flex justify-between">
+        <div
+          className="flex items-center space-x-4 mb-2 cursor-pointer"
+          onClick={() => router.push(`/job/${job._id}`)}
+        >
+          <Image
+            alt={`logo`}
+            src={job.createdBy.profilePicture || "/user.png"}
+            width={48}
+            height={48}
+            className="rounded-full shadow-sm"
+          />
 
-        <div>
-          <CardTitle className="text-xl font-bold truncate">
-            {job.title}
-          </CardTitle>
-          <p className="text-sm text-muted-foreground">{job.createdBy.name}</p>
+          <div>
+            <CardTitle className="text-xl font-bold truncate">
+              {job.title}
+            </CardTitle>
+            <p className="text-sm text-muted-foreground">
+              {job.createdBy.name}
+            </p>
+          </div>
         </div>
+        <button
+          className={`text-2xl ${
+            isLiked ? "text-[#7263f3]" : "text-gray-400"
+          } `}
+          onClick={() => {
+            isAuthenticated
+              ? handleLike(job._id)
+              : router.push("https://jobfindr-q1cl.onrender.com/login");
+          }}
+        >
+          {isLiked ? bookmark : bookmarkEmpty}
+        </button>
       </div>
       <div>
         <p className="text-sm text-muted-foreground mb-2">{job.location}</p>
