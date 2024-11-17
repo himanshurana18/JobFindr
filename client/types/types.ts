2b7f9d197d10a522c8438813ebf879d7349b8ede
maclinz <tafadzwamucheke@gmail.com>
2024-11-17 17:05:12 +0000

    feat: initial commit

---
+++ client/types/types.ts
@@ -0,0 +1,21 @@
+interface Job {
+  _id: string;
+  title: string;
+  description: string;
+  location: string;
+  salary: number;
+  salaryType: "Yearly" | "Monthly" | "Weekly" | "Hourly";
+  negotiable: boolean;
+  jobType: string[];
+  tags: string[];
+  likes: string[];
+  skills: string[];
+  applicants: string[];
+  createdBy: {
+    profilePicture: string;
+    name: string;
+  };
+  createdAt: string;
+  updatedAt: string;
+}
+export type { Job };
